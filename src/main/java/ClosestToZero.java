9d6011089f97855372224b3b2415e59c1efe992d
fallahm95 <mahdyfalah@gmail.com>
2022-03-06 11:22:49 +0100

    added findClosestToZeroTie() to test a list with tie added javadoc for ClosestToZero class changed groupId and artifactId in pom.xml some fixes for ClosestToZeroTest to avoid repetition of ClosestToZero class inside functions

--- src/main/java/ClosestToZero.java
+++ src/main/java/ClosestToZero.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * This class finds closest value to zero in a list, or the positive value in case of tie
+ */
 public class ClosestToZero {
-    //this function find closest value to zero in a list, or the positive value in case of tie
-    public static int find(ArrayList<Integer> list) {
 
-        //if conditions to prevent errors
+    /**
+     * gets the list of integers as parameter, if the list is empty 0 is returned
+     * if the list does not contain 0, zero is added, then the list is sorted
+     * and checks which positive or negative number is closest to zero
+     *
+     * @param list
+     * @return int
+     */
+    public static int find(ArrayList<Integer> list) {
         if (list.isEmpty()) {
             return 0;
         }
@@ -14,25 +23,19 @@ public class ClosestToZero {
         }
 
         Collections.sort(list);
-        int res = 0;
+        int result = 0;
 
-        // if list contains positive and negative values closest values are
-        // one before and one after zero in sorted list
         if (list.indexOf(0) > 1 && list.indexOf(0) < list.size() - 1) {
             int negativeNeighbour = list.get(list.indexOf(0) - 1);
             int positiveNeighbour = list.get(list.indexOf(0) + 1);
 
-            res = Math.abs(negativeNeighbour) < positiveNeighbour ? negativeNeighbour : positiveNeighbour;
-        }
-        // if no negative value exists the closest value is one after zero
-        else if (list.indexOf(0) == 0) {
-            res = list.get(list.indexOf(0) + 1);
-        }
-        // if no positive value exists the closest value is one before zero
-        else {
-            res = list.get(list.indexOf(0) - 1);
+            result = Math.abs(negativeNeighbour) < positiveNeighbour ? negativeNeighbour : positiveNeighbour;
+        } else if (list.indexOf(0) == 0) {
+            result = list.get(list.indexOf(0) + 1);
+        } else {
+            result = list.get(list.indexOf(0) - 1);
         }
 
-        return res;
+        return result;
     }
 }
--- src/test/java/ClosestToZeroTest.java
+++ src/test/java/ClosestToZeroTest.java
@@ -6,27 +6,30 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClosestToZeroTest {
+    ClosestToZero closestToZero = new ClosestToZero();
+
     @Test
     void findClosestToZeroInList() {
-        var closestToZero = new ClosestToZero();
         assertEquals(-2, closestToZero.find(new ArrayList<>(List.of(11, 3, -3, -6, -2))));
     }
 
     @Test
     void findClosestToZeroInListNoPositive() {
-        var closestToZero = new ClosestToZero();
         assertEquals(-2, closestToZero.find(new ArrayList<>(List.of(-3, -6, -2))));
     }
 
     @Test
     void findClosestToZeroInListNoNegative() {
-        var closestToZero = new ClosestToZero();
         assertEquals(2, closestToZero.find(new ArrayList<>(List.of(11, 3, 2))));
     }
 
     @Test
     void findClosestToZeroInEmptyList() {
-        var closestToZero = new ClosestToZero();
         assertEquals(0, closestToZero.find(new ArrayList<>()));
+    }
+
+    @Test
+    void findClosestToZeroTie() {
+        assertEquals(3, closestToZero.find(new ArrayList<>(List.of(11, 3, -3, -6))));
     }
 }
